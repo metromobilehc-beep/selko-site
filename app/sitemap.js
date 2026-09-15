cd8dff903a0280ac27bf429d06d64676a46b85f2
metromobilehc-beep <metromobilehc@gmail.com>
2026-09-14 20:49:05 -0500

    Add files via upload

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,6 +1,6 @@
 export default function sitemap() {
   const base = 'https://selko360.com';
-  const routes = ['', '/cred', '/comply'];
+  const routes = ['', '/cred', '/comply', '/demo'];
 
   return routes.map((route) => ({
     url: `${base}${route}`,
